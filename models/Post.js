e4413616e7d6c07666a91ce737053f24e1cbbd97
FSDelite <felipedelite@gmail.com>
2021-09-02 09:46:32 -0400

    post commit

---
+++ models/Post.js
@@ -0,0 +1,14 @@
+const { Model } = require("sequelize/types");
+const db = require("./db");
+
+const Post = db.sequelize.define("postagens",{
+    titulo: {
+        type: db.Sequelize.STRING
+    },
+    conteudo:{
+        type: db.Sequelize.STRING
+    }
+})
+
+//Post.sync({force:true}); Já criei a tabela, por isso comentei
+module.exports = Post;
